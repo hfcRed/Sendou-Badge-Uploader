f6d1072163ba143887e1fa85c539771a9f75048e
hfcRed <hfcred@gmx.net>
2026-07-31 15:16:27 -0400

    Update constants to point at new repo

--- src/lib/server/github/constants.ts
+++ src/lib/server/github/constants.ts
@@ -16,8 +16,8 @@ const CALLBACK_URL = dev
 export const BASE_URL = 'https://api.github.com';
 export const BASE_BRANCH = dev ? 'main' : 'main';
 export const REPO_OWNER = dev ? 'hfcRed' : 'sendou-ink';
-export const REPO_NAME = dev ? 'PR-Testing' : 'sendou.ink';
-export const FILE_PATH = dev ? 'homemade.json' : 'app/features/badges/homemade.json';
-export const DIRECTORY = dev ? 'badges' : 'public/static-assets/badges';
+export const REPO_NAME = dev ? 'PR-Testing' : 'assets';
+export const FILE_PATH = 'homemade.json';
+export const DIRECTORY = dev ? 'badges' : 'assets/badges';
 
 export const githubClient = new GitHub(CLIENT_ID, CLIENT_SECRET, CALLBACK_URL);
