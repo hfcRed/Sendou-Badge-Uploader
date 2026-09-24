c559ae26711406f90fe3114584e7a75bf3b85783
hfcRed <hfcred@gmx.net>
2026-09-24 10:16:55 -0400

    Add deprecation notice

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -47,9 +47,26 @@
 	<h1>Upload Badges</h1>
 	<p>Generate badge files and create Pull Requests automatically</p>
 </hgroup>
+<hgroup>
+	<h2>DEPRECATION NOTICE!</h2>
+	<p>
+		Badge uploading will be DISCONTINUED starting October 1st! If you want to add a reward to your
+		tournament in the future, make a <a href="https://sendou.ink/trophies/new" target="_blank"
+			>Trophy</a
+		> instead.
+	</p>
+</hgroup>
+
 <hr />
 <Viewer />
 <hr />
 <DataForm bind:shorthandName />
 <hr />
 <PRForm {shorthandName} {submitting} />
+
+<style>
+	h2,
+	h2 + p {
+		color: var(--pico-del-color);
+	}
+</style>
